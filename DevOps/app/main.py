979c2b245d7143de8876438c03fd2a37b38bb0f8
igor-timchenko <74782054+igor-timchenko@users.noreply.github.com>
2025-01-31 17:09:05 +0700

    Update main.py
    -2-

--- DevOps/app/main.py
+++ DevOps/app/main.py
@@ -1,37 +1,29 @@
 import psycopg2
 import os
-
 def main():
     # Получаем параметры подключения из переменных окружения
     db_host = os.getenv('DB_HOST', 'db')
     db_name = os.getenv('DB_NAME', 'mydatabase')
     db_user = os.getenv('DB_USER', 'postgres')
     db_password = os.getenv('DB_PASSWORD', 'password')
-
     # Подключаемся к базе данных
     connection = psycopg2.connect(
         host=db_host,
         database=db_name,
         user=db_user,
         password=db_password
-    )
-    
-    cursor = connection.cursor()
-    
+    )  
+    cursor = connection.cursor()   
     # Выполняем запрос к таблице
-    cursor.execute("SELECT message FROM test_table;")
-    
+    cursor.execute("SELECT message FROM test_table;") 
     # Получаем результат
-    row = cursor.fetchone()
-    
+    row = cursor.fetchone()  
     if row:
         print(row[0])
     else:
         print("No data found.")
-    
     # Закрываем соединение
     cursor.close()
     connection.close()
-
 if __name__ == "__main__":
     main()
