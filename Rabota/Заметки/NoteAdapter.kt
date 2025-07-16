78e38d1febf02838eff82bca48b01e9317ee7d4d
igor-timchenko <74782054+igor-timchenko@users.noreply.github.com>
2025-07-16 21:04:12 +0700

    Create NoteAdapter.kt

---
+++ Rabota/Заметки/NoteAdapter.kt
@@ -0,0 +1,41 @@
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteAdapter(private val onClick: (Note) -> Unit) :
+    ListAdapter<Note, NoteAdapter.NoteViewHolder>(NoteDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return NoteViewHolder(ItemNoteBinding.inflate(inflater, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class NoteViewHolder(private val binding: ItemNoteBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(note: Note) {
+            binding.noteTitle.text = note.title
+            binding.noteContent.text = note.content
+            binding.noteDate.text = SimpleDateFormat("dd.MM.yyyy HH:mm", Locale.getDefault())
+                .format(note.date)
+            
+            binding.root.setOnClickListener { onClick(note) }
+        }
+    }
+}
+
+class NoteDiffCallback : DiffUtil.ItemCallback<Note>() {
+    override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+        return oldItem == newItem
+    }
+}
