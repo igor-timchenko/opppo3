b57edf451890729ca33dc15dacca9770bfac276d
igor-timchenko <74782054+igor-timchenko@users.noreply.github.com>
2025-07-16 21:00:30 +0700

    Create NoteDao.kt

---
+++ Rabota/Заметки/NoteDao.kt
@@ -0,0 +1,17 @@
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface NoteDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(note: Note)
+
+    @Update
+    suspend fun update(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("SELECT * FROM notes ORDER BY date DESC")
+    fun getAllNotes(): LiveData<List<Note>>
+}
